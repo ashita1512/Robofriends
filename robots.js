0388bb4299dc3e077f5ed1ae8ae591b1457c0a7b
Ashita <98086953+ashita1512@users.noreply.github.com>
2022-04-08 18:03:00 +0530

    Add files via upload

---
+++ robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+        id: 1,
+        name: "Zita",
+        username: "zita",
+        email: "zita@gmail.com"
+    },
+    {
+        id: 2,
+        name: "Chumchum",
+        username: "chums",
+        email: "chums@gmail.com"
+    },
+    {
+        id: 3,
+        name: "Ixuda",
+        username: "ix",
+        email: "ix@gmail.com"
+    },
+    {
+        id: 4,
+        name: "Sucker",
+        username: "suck",
+        email: "suck@gmail.com"
+    },
+    {
+        id: 5,
+        name: "karan",
+        username: "karan",
+        email: "karan@gmail.com"
+    },
+    {
+        id: 6,
+        name: "arjun",
+        username: "arjun",
+        email: "arjun@gmail.com"
+    },
+    {
+        id: 7,
+        name: "nagin",
+        username: "nagin",
+        email: "nagin@gmail.com"
+    },
+    {
+        id: 8,
+        name: "babs",
+        username: "babs",
+        email: "babs@gmail.com"
+    },
+    {
+        id: 9,
+        name: "bhaiya",
+        username: "bhaiya",
+        email: "bhaiya@gmail.com"
+    },
+    {
+        id: 10,
+        name: "Eetuuu",
+        username: "Eetuu",
+        email: "eetuu@gmail.com"
+    }
+
+]
